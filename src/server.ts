59aeca7459a3174fab53feccce78917b19c2d501
Kaian Samuel <kaiansamuelster@gmail.com>
2024-12-07 07:23:53 -0300

    Route params, query params, dados no body

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,35 @@
-function sum(a: number, b:number): number {
-  return a + b
-}
+import express from "express"
 
-const result: number = sum(5, 7)
-console.log("Resultado da soma ", result)
+const PORT = 3333
+
+const app = express()
+app.use(express.json())   
+
+app.get("/", (request, response) => {
+  response.end("Hello World Express")
+})
+
+//Route params
+//app.get("/products/:id", (request, response) => {
+//  const { id } = request.params
+//
+//  response.send(`Produto ${id}`)
+//})
+
+//Parametros não nomeados
+app.get("/products", (request, response) =>{
+  const { page, limit } = request.query
+
+  response.send(`Página ${page} de ${limit}`)
+})
+
+app.post("/products", (request, response) => {
+  const { name, price } = request.body
+
+  //response.end(`Produto ${name} custa $ ${price}`)
+  response.status(201).json({ name, price})
+})
+
+app.listen(PORT, () => console.log(`Server running at ${PORT}`))
+
+
